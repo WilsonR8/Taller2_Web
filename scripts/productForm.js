666da171173d3b626dc603e2532cdd54cc8f1d53
Wilson <wilsonst.suarez@hotmail.com>
2021-05-21 16:14:17 -0500

    Product cards ready

--- scripts/productForm.js
+++ scripts/productForm.js
@@ -1,16 +1,3 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyB_yqeeUcy1MciBK3RDe6pusFfskQ56tMs",
-  authDomain: "taller2-web-40ef9.firebaseapp.com",
-  projectId: "taller2-web-40ef9",
-  storageBucket: "taller2-web-40ef9.appspot.com",
-  messagingSenderId: "121288523965",
-  appId: "1:121288523965:web:cd2316c0df6282adbeb7d4"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
-const storage = firebase.storage();
 
 const productForm = document.querySelector('.productForm');
 const productFormLoading = document.querySelector('.productForm__loading');
--- scripts/store.js
+++ scripts/store.js
@@ -1,51 +1,28 @@
-
-const items = [
-    {
-        img: "https://images-na.ssl-images-amazon.com/images/I/71MfPzyRUcL._AC_SX569_.jpg",
-        title: 'Lorem Ipsum',
-        price: 60000,
-
-    },
-
-    {
-        img: "https://images-na.ssl-images-amazon.com/images/I/71MfPzyRUcL._AC_SX569_.jpg",
-        title: 'Lorem Ipsum',
-        price: 70000,
-
-    },
-
-    {
-        img: "https://images-na.ssl-images-amazon.com/images/I/71MfPzyRUcL._AC_SX569_.jpg",
-        title: 'Lorem Ipsum',
-        price: 80000,
-
-    },
-];
-
 const list = document.querySelector('.list');
 
-function handleProductItem(item) {
-    const items = document.createElement('a');
-    items.innerHTML = `
-    <img class="items__img"
-    src="${item.img}">
-<div class="items__info">
-    <h1 class="items__title">
-    ${item.title}
-    </h1>
-    <h3 class="items__price">
-    ${item.price}
-    </h3>
-</div>
-</a>
-   `;
+db.collection('products')
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            const items = document.createElement('a');
+            let img = data.images[0]?.url;
+            if(!img){
+                img='./img/placeholder.png';
+            }
+            items.innerHTML = `
+            <img class="items__img" src="${img}">
+            <div class="items__info">
+                <h1 class="items__title">${data.name}</h1>
+                <h3 class="items__price">${data.price}</h3>
+            </div>
+           `;
+        
+            items.classList.add('items');
+            items.setAttribute('href', 'a');
+            list.appendChild(items);
 
-    items.classList.add('items');
-    items.setAttribute('href','a');
-    list.appendChild(items);
+        });
+    })
 
-    console.log(items);
-}
-
-items.forEach(handleProductItem)
 
